120e6609584b2ff41c8ccf15bde68de9eba50ba8
Túlio de Oliveira <tulio.ao@gmail.com>
2017-10-06 11:20:40 -0300

    Database Setup

---
+++ cria_dist.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sqlite3
+import db
+import simplejson, urllib.request
+
+db=db.DB('database.sqlite')
+
+states = db.get_states()
+
+stateId = input("Insert State Id: ")
+
+cityCount = db.get_number_cities(stateId)
+print("Number of cities in state:", cityCount)
+
+cities = db.get_cities(stateId)
+
+initialIndex = int(input("Starting index for cities: "))
+secondaryIndex = int(input("Starting index for second city: "))
+
+# 6, 137
+
+if (secondaryIndex == initialIndex):
+    db.insert_distance_row(cities[initialIndex]['id'], cities[initialIndex]['id'], 0)
+    secondaryIndex += 1
+
+for j in range (secondaryIndex, cityCount):
+    print("city1Id (index):", cities[initialIndex]['id'], '(',initialIndex,')', "city2Id (index):", cities[j]['id'], '(',j,')')
+    orig = "%f,%f" % (cities[initialIndex]['lat'], cities[initialIndex]['lon'])
+    dest = "%f,%f" % (cities[j]['lat'], cities[j]['lon'])
+    url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&language=en-EN&sensor=false".format(str(orig), str(dest))
+    result = simplejson.load(urllib.request.urlopen(url))
+    driving_distance = result['rows'][0]['elements'][0]['distance']['value'] / 1000.0
+    db.insert_distance_row(cities[initialIndex]['id'], cities[j]['id'], driving_distance)
+        
+for i in range(initialIndex+1, cityCount):
+    db.insert_distance_row(cities[i]['id'], cities[i]['id'], 0)
+    for j in range (i+1, cityCount):
+        print("city1Id (index):", cities[i]['id'], '(',i,')', "city2Id (index):", cities[j]['id'], '(',j,')')
+        orig = "%f,%f" % (cities[i]['lat'], cities[i]['lon'])
+        dest = "%f,%f" % (cities[j]['lat'], cities[j]['lon'])
+        url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&language=en-EN&sensor=false".format(str(orig), str(dest))
+        result = simplejson.load(urllib.request.urlopen(url))
+        driving_distance = result['rows'][0]['elements'][0]['distance']['value'] / 1000.0
+        db.insert_distance_row(cities[i]['id'], cities[j]['id'], driving_distance)
+
+db.conn.commit()
---
+++ db.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import sqlite3
+
+
+class DB(object):
+    def __init__(self, fileName):
+        self.db_connect(fileName)
+
+    def __del__(self):
+        self.conn.close()
+
+    def db_connect(self, fileName):
+        if (os.path.isfile(fileName) == False):
+           print("sqlite database file {:s} not found!".format(fileName))
+           sys.exit(-1)
+        self.conn = sqlite3.connect(fileName)
+
+    def get_states(self):
+        cur = self.conn.cursor()
+        cur.execute("select id, uf from states")
+        states = [[row[0], str(row[1])] for row in cur.fetchall()]
+        return states
+
+    def get_cities(self, ufid):
+        cur = self.conn.cursor()
+        cur.execute(
+            "select * from cities where state_id = '{_ufid}'".format(_ufid=ufid))
+        cities = [{"id": row[0], "name": row[3], "lat": row[4],
+                   "lon": row[5], "pop": row[6]} for row in cur.fetchall()]
+        return cities
+
+    def get_number_cities(self, ufid):
+        cur = self.conn.cursor()
+        cur.execute(
+            "select count(*) from cities where state_id = '{_ufid}'".format(_ufid=ufid))
+        return cur.fetchone()[0]
+
+    def get_number_ranged_cities(self, ufid, pop_a=0, pop_b=sys.maxsize):
+        cur = self.conn.cursor()
+        cur.execute("select count(*) from cities where state_id = '{_ufid}' and pop >= {_pop_a} and pop <= {_pop_b}".format(
+            _ufid=ufid, _pop_a=pop_a, _pop_b=pop_b))
+        return cur.fetchone()[0]
+
+    def get_ranged_cities(self, ufid, pop_a=0, pop_b=sys.maxsize):
+        cur = self.conn.cursor()
+        cur.execute("select * from cities where state_id = '{_ufid}' and pop >= {_pop_a} and pop <= {_pop_b}".format(
+            _ufid=ufid, _pop_a=pop_a, _pop_b=pop_b))
+        cities = [{"id": row[0], "name": row[3], "lat": row[4],
+                   "lon": row[5], "pop": row[6]} for row in cur.fetchall()]
+        return cities
+
+    def get_max_pop(self, ufid):
+        cur = self.conn.cursor()
+        cur.execute(
+            "select max(pop) from cities where state_id = '{_ufid}'".format(_ufid=ufid))
+        return cur.fetchone()[0]
+
+    def get_min_pop(self, ufid):
+        cur = self.conn.cursor()
+        cur.execute(
+            "select min(pop) from cities where state_id = '{_ufid}'".format(_ufid=ufid))
+        return cur.fetchone()[0]
+
+    def create_distance_table(self):
+        cur = self.conn.cursor()
+        
+        sql = "CREATE TABLE IF NOT EXISTS 'dist' ( 'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 'city1_id' INTEGER NOT NULL, 'city2_id' INTEGER NOT NULL, 'distance' REAL NOT NULL, FOREIGN KEY('city1_id') REFERENCES 'cities'('id'), FOREIGN KEY('city2_id') REFERENCES 'cities'('id') )"
+        cur.execute(sql)
+        self.conn.commit()
+
+    def insert_distance_row(self, city1Id, city2Id, distance):
+        cur = self.conn.cursor()
+        sql = "INSERT INTO dist (city1_id, city2_id, distance)" + \
+            "VALUES ({_city1Id}, {_city2Id}, {_distance})".format(
+                _city1Id=city1Id, _city2Id=city2Id, _distance=distance)
+        cur.execute(sql)
+        self.conn.commit()
