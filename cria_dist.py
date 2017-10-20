8b21150bd0ef8b733ba1c7cc4409e2530ef3ed22
Túlio de Oliveira <tulio.ao@gmail.com>
2017-10-20 11:19:22 -0200

    up

--- cria_dist.py
+++ cria_dist.py
@@ -19,7 +19,7 @@ cities = db.get_cities(stateId)
 initialIndex = int(input("Starting index for cities: "))
 secondaryIndex = int(input("Starting index for second city: "))
 
-# 6, 137
+#40 235
 
 if (secondaryIndex == initialIndex):
     db.insert_distance_row(cities[initialIndex]['id'], cities[initialIndex]['id'], 0)
@@ -29,8 +29,9 @@ for j in range (secondaryIndex, cityCount):
     print("city1Id (index):", cities[initialIndex]['id'], '(',initialIndex,')', "city2Id (index):", cities[j]['id'], '(',j,')')
     orig = "%f,%f" % (cities[initialIndex]['lat'], cities[initialIndex]['lon'])
     dest = "%f,%f" % (cities[j]['lat'], cities[j]['lon'])
-    url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&language=en-EN&sensor=false".format(str(orig), str(dest))
+    url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&language=en-EN&sensor=false&key=AIzaSyCdUNvXpeJOcbVkpftzw95h2Ip6P_eQP_c".format(str(orig), str(dest))
     result = simplejson.load(urllib.request.urlopen(url))
+    print(result)
     driving_distance = result['rows'][0]['elements'][0]['distance']['value'] / 1000.0
     db.insert_distance_row(cities[initialIndex]['id'], cities[j]['id'], driving_distance)
         
@@ -40,8 +41,9 @@ for i in range(initialIndex+1, cityCount):
         print("city1Id (index):", cities[i]['id'], '(',i,')', "city2Id (index):", cities[j]['id'], '(',j,')')
         orig = "%f,%f" % (cities[i]['lat'], cities[i]['lon'])
         dest = "%f,%f" % (cities[j]['lat'], cities[j]['lon'])
-        url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&language=en-EN&sensor=false".format(str(orig), str(dest))
+        url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={0}&destinations={1}&mode=driving&language=en-EN&sensor=false&key=AIzaSyCdUNvXpeJOcbVkpftzw95h2Ip6P_eQP_c".format(str(orig), str(dest))
         result = simplejson.load(urllib.request.urlopen(url))
+        print(result)
         driving_distance = result['rows'][0]['elements'][0]['distance']['value'] / 1000.0
         db.insert_distance_row(cities[i]['id'], cities[j]['id'], driving_distance)
 
